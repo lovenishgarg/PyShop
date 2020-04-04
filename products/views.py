68fb4ebfbdc6a0a4fa6ebe9d4e587e9188dc0256
Lovenish Garg <60829596+techielove92@users.noreply.github.com>
2020-04-04 01:55:30 -0700

    Shopping market

---
+++ products/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+# Create your views here.
+#/products -> index
+# uniform resource Locator(Address)
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html',
+                  {'products' : products})
+
+
+def new(reques):
+    return HttpResponse('New Prodcut')
+
+
+
+
+
